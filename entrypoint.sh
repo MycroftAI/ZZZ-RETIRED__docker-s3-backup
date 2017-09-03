9d80a69d4603fb368676c0e0a942d0e0551e05ec
aatchison <arron.atchison@gmail.com>
2017-09-04 04:09:28 +0900

    changed backups.mycroft.team uri to ssl:

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,12 +69,12 @@ function backup_operation {
 		export EMAIL_BODY=${EMAIL_SUBJECT}
 		curl -i -X POST -d 'payload={"text": ":white_check_mark: Backup to s3 succesful for host: '"${s3_bucket_path}"'"}' https://chat.mycroft.ai/hooks/bbutxxwu8fgm8cqa6wh6eaq7ze
 		$(python mailer.py)
-		 curl -H "Content-Type: application/json" -X POST -d '{"hostname":"'${s3_bucket_path}'", "desc":"description coming soon!", "time": "'${timestamp}'", "success":"0"}' http://backups.mycroft.team:9898/nodes
+		 curl -H "Content-Type: application/json" -X POST -d '{"hostname":"'${s3_bucket_path}'", "desc":"description coming soon!", "time": "'${timestamp}'", "success":"0"}' https://backups.mycroft.team/nodes
 	else
 		export exit_status=${exit_state}
 		echo "Error detected, operation exited: "${exit_state}
 		curl -i -X POST -d 'payload={"text": "@here :x: Backup to s3 failed for host: '"${s3_bucket_path}"' with exit status '"${exit_state}"'" }' https://chat.mycroft.ai/hooks/bbutxxwu8fgm8cqa6wh6eaq7ze
-		curl -H "Content-Type: application/json" -X POST -d '{"hostname":"'${s3_bucket_path}'", "desc":"description coming soon!", "time": "'${timestamp}'", "success":"1"}' http://backups.mycroft.team:9898/nodes
+		curl -H "Content-Type: application/json" -X POST -d '{"hostname":"'${s3_bucket_path}'", "desc":"description coming soon!", "time": "'${timestamp}'", "success":"1"}' https://backups.mycroft.team/nodes
 	fi
 	echo ${s3_bucket_path}" " ${date_today}" " ${timestamp}" "${exit_status} >> /s3-mount/${s3_bucket_path}/tar-incremental/${date_today}/${date_today}-${s3_bucket_path}.log
 
